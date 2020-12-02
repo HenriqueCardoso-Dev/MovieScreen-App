5618c29938d6502778183d8f2a2a3af56ff6735a
Carlos Henrique Ramos Cardoso <63278045+HenriqueCardoso-Dev@users.noreply.github.com>
2020-12-02 20:15:00 -0300

    0.2.0

--- moviescreen/src/app/app.component.ts
+++ moviescreen/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -10,12 +10,25 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  
   navegacao : any;
+  fotoPerfil:"";
+  
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private route:ActivatedRoute
   ) {
+
+    route.queryParams.subscribe(params => {
+      if (params && params.special){
+        //conversão de JSON para string
+        let loginData = JSON.parse(params.special);
+        this.fotoPerfil = loginData.fotoPerfil;
+      }
+    });
+
     this.initializeApp();
     this.sideMenu();
   }
@@ -31,7 +44,7 @@ export class AppComponent {
   {
     this.navegacao =
     [
-      {title:"Perfil", url:"/home", icon:"../assets/ion-icons/happy-outline.svg"},
+      {title:"Perfil", url:"/home", icon:"../assets/ion-icons/person-outline.svg"},
       {title:"Filmes", url:"/movies", icon:"../assets/ion-icons/film-outline.svg"},
       {title:"Séries", url:"/series", icon:"../assets/ion-icons/planet-outline.svg"},
       {title:"Animes", url:"/animes", icon:"../assets/ion-icons/nutrition-outline.svg"}
